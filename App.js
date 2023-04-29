20718fc7a565154a43e96eb02e4535d7c5833a2d
EdCarlos-dev <bio_edc@hotmail.com>
2023-04-28 21:55:04 -0300

    Alteraçoes no app

--- App.js
+++ App.js
@@ -47,21 +47,7 @@ const App = () => (
       />
     </View>
     <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
-      </View>
-    </View>
+    
   </SafeAreaView>
 );
 
